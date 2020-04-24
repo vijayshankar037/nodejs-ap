cf307bc4ffda28be4d17b9f249e59fe00bd1251a
vijayshankar037 <vijayshankar1990@hotmail.com>
2020-04-24 14:00:40 +0530

    express imported

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,7 @@
-console.log("Called index")
+import * as express from 'express';
+
+let app: express.Application = express();
+
+app.listen(5000,() => {
+  console.log("server listing on port 5000")
+})
