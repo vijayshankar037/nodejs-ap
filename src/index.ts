966fb5c448471c675548302385814abcd55571ee
vijayshankar037 <vijayshankar1990@hotmail.com>
2020-04-24 14:45:53 +0530

    added routing

--- src/index.ts
+++ src/index.ts
@@ -4,4 +4,15 @@ let app: express.Application = express();
 
 app.listen(5000,() => {
   console.log("server listing on port 5000")
+});
+
+app.get("/login",(req,res)=>{
+  const data = {first_name: 'vijay', last_name:'shankar'}
+  //res.send('success');
+  //res.json([{firstName: "Joney",lastName: "Singh"},{firstName: "Jon",lastName: "kumar"}]) 
+  res.send(data);
+})
+
+app.get("/test",(req,res)=>{
+  res.send("testing route");
 })
